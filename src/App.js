4ed91eb39d596b7030d4140e41e28446e20c5f94
Michael Rafferty <Michael.Rafferty@macys.com>
2022-07-24 10:21:12 -0400

    Configure React app for deployment to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { Route } from "react-router";
+import { Link } from "react-router-dom";
+
+function Home() {
+  return <h1>Welcome to the home page</h1>;
+}
+
+function About() {
+  return <h1>You are on the about page</h1>;
+}
+
+function Contact() {
+  return <h1>Please feel free to email us</h1>;
+}
+
+function NoMatch() {
+  return <h1>404 Not Found</h1>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+
+          <Route exact path="/">
+            <Home />
+          </Route>
+
+          <Route path="/About">
+            <About />
+          </Route>
+
+          <Route path="/contact">
+            <Contact />
+          </Route>
+
+          <Route>
+            <NoMatch />
+          </Route>
+
+      </div>
   );
 }
 
